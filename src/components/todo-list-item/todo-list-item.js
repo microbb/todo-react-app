49c1c4887ac1e02f4cd4406fb36b48be32c9f2fc
Вадим Арслангалин <vadimarslangalin@MacBook-Pro-Vadim.local>
2021-08-04 14:06:48 +0500

    добавил state, в функции которая срабатывает на клик меняю состояние state, написал условие по которому добавляется нужный класс

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,19 +3,32 @@ import React, {Component} from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
+  state = {
+    done: false
+  };
 
-  onLabelClick = () => console.log(`Done: ${this.props.label}`)
+  onLabelClick = () => {
+    this.setState({
+      done: true
+    })
+  }
 
   render() {
     const { label, important = false } = this.props;
+    const { done } = this.state;
 
     const style = {
       color: important ? 'steelblue' : 'black',
       fontWeight: important ? 'bold' : 'normal'
     };
 
+    let classNames = 'todo-list-item'
+    if (done) {
+      classNames += ' done'
+    }
+
     return (
-      <span className="todo-list-item">
+      <span className={ classNames }>
       <span
         className="todo-list-item-label"
         style={style}
