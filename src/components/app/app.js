c33596905691151b7ec0310ff8df919b22ea3c57
Вадим Арслангалин <vadimarslangalin@MacBook-Pro-Vadim.local>
2021-08-04 19:24:21 +0500

    Центрилизовал данные, теперь все находится в App, из за этого изменил компонент TodoListItem, исключил дублирование кода, разобрался как менять элементы в state

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,11 +14,20 @@ export default class App extends Component {
 
   state = {
     todoData: [
-      { label: 'Drink Coffee', important: false, id: 1 },
-      { label: 'Make Awesome App', important: true, id: 2 },
-      { label: 'Have a lunch', important: false, id: 3 }
+      this.createTodoItem('Drink Coffee'),
+      this.createTodoItem('Make Awesome App'),
+      this.createTodoItem('Have a lunch'),
     ]
   };
+
+  createTodoItem(label) {
+    return {
+      label,
+      important: false,
+      done: false,
+      id: this.maxID++
+    };
+  }
 
   deletedItem = (id) => {
     this.setState(({ todoData }) => {
@@ -36,11 +45,7 @@ export default class App extends Component {
   }
 
   addItem = (text) => {
-    const newItem = {
-      label: text,
-      important: false,
-      id: this.maxID++
-    }
+    const newItem = this.createTodoItem(text);
 
     this.setState(({ todoData }) => {
 
@@ -55,18 +60,58 @@ export default class App extends Component {
     });
   }
 
+  toggleProperty(arr, id, propName) {
+    const idx = arr.findIndex(el => el.id === id);
+
+    // update obj
+    const oldItem = arr[idx];
+    const newItem = {
+      ...oldItem,
+      [propName]: !oldItem[propName]
+    };
+
+    // construct new arr
+    return [
+      ...arr.slice(0, idx),
+      newItem,
+      ...arr.slice(idx + 1)
+    ]
+  }
+
+  onToggleImportant = (id) => {
+    this.setState(({ todoData }) => {
+      return {
+        todoData: this.toggleProperty(todoData, id, 'important')
+      }
+    });
+  }
+
+  onToggleDone = (id) => {
+    this.setState(({ todoData }) => {
+      return {
+        todoData: this.toggleProperty(todoData, id, 'done')
+      }
+    });
+  }
+
   render() {
+    const { todoData } = this.state;
+    const doneCount = todoData.filter(el => el.done).length;
+    const todoCount = todoData.length - doneCount;
+
     return (
       <div className="todo-app">
-        <AppHeader toDo={1} done={3} />
+        <AppHeader toDo={ todoCount } done={ doneCount } />
         <div className="top-panel d-flex">
           <SearchPanel />
           <ItemStatusFilter />
         </div>
 
         <TodoList
-          todos={this.state.todoData}
-          onDeleted={ this.deletedItem } />
+          todos={ todoData }
+          onDeleted={ this.deletedItem }
+          onToggleImportant={ this.onToggleImportant }
+          onToggleDone={ this.onToggleDone } />
         <ItemAddForm onItemAdded={ this.addItem }/>
       </div>
   );
