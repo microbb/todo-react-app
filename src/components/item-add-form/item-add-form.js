e0cdf386b846a446f4a336558cc08a7fda30ef4c
Вадим Арслангалин <vadimarslangalin@MacBook-Pro-Vadim.local>
2021-08-06 15:08:09 +0500

    item-add-form теперь компонент класса, добавил 2 функции с помошью которых можно будет пеердавать значение в itemAdded, добавил немного стилей

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './item-add-form.css'
 
-const ItemAddForm = ({ onItemAdded }) => {
-  return (
-    <div className='item-add-form'>
-      <button
-        className='btn btn-outline-secondary'
-        onClick={ () => onItemAdded('hello') } >
-        Добавить
-      </button>
-    </div>
-  );
-}
+export default class ItemAddForm extends Component {
 
-export default ItemAddForm;
+  // Чтобы удобно было отслеживать изменение и его записывать
+  state = {
+    label: ''
+  }
+
+  // Функция которая занимается изменением label в state
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+  }
+
+  render() {
+    return (
+      <form className='item-add-form d-flex'
+            onSubmit={ this.onSubmit } >
+        <input type="text" className='form-control'
+        placeholder='what needs to be done'
+        onChange={ this.onLabelChange } />
+        <button
+          className='btn btn-outline-secondary' >
+          Добавить
+        </button>
+      </form>
+    );
+  }
+}
